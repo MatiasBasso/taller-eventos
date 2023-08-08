37ebd951a4196b59f7b1405895c099688cf72283
MatiasBasso <matiasbasso01@gmail.com>
2023-08-08 12:29:29 -0300

    Se agrego alerta en el Div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const divElement = document.getElementById('myDiv');
+
+    divElement.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
+
